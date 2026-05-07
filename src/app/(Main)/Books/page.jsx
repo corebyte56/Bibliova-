6d1c35df3f380c6060cf2ff9e619ffc6493ff91d
shahariar ahammad fahim <fahim789m@gmail.com>
2026-05-07 16:04:31 +0600

    Add active navbar links and update all books section

--- src/app/(Main)/Books/page.jsx
+++ src/app/(Main)/Books/page.jsx
@@ -1,7 +1,37 @@
+import BookCards from "@/app/UI/BookCards";
+import NavLink from "@/components/NavLink";
+import { Button } from "@heroui/react";
+
 export default function BooksPage() {
+  async function getBooks() {
+  const res = await fetch("http://localhost:5001/books", {
+    cache: "no-store",
+  });
+
+  return res.json();
+}
   return (
     <div>
-      <h1>Books Page</h1>
+      <div className="container mx-auto px-4 py-12">
+      <h2 className="text-2xl font-bold mb-5">Featured Books</h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {getBooks().then((books) => books.map((book) => (
+          <BookCards key={book.id} book={book} />
+        )))}
+      </div>
+
+      <div className="mt-6 text-center">
+        <NavLink
+          href="/Books"
+          className="px-5 py-2 bg-black text-white rounded"
+        >
+          <Button className="px-5 py-2 bg-black text-white rounded">
+            View All Books
+          </Button>
+        </NavLink>
+      </div>
+    </div>
     </div>
   );
 }
--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,15 +1,25 @@
-'use client';
+"use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const NavLink = ({ href, children }) => {
+const NavLink = ({ href, children, className = "" }) => {
+  const pathname = usePathname();
+
+  const isActive = pathname === href;
+
   return (
-    <div>
-      <Link href={href}>
-        {children}
-      </Link>
-    </div>
-  )
-}
+    <Link
+      href={href}
+      className={`
+        transition-all duration-300
+        ${isActive ? "text-[#EF9F27]" : "text-inherit"}
+        ${className}
+      `}
+    >
+      {children}
+    </Link>
+  );
+};
 
-export default NavLink
+export default NavLink;
