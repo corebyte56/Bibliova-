d89d78891e745a0871e7098041e611bad7b0d5d4
shahariar ahammad fahim <fahim789m@gmail.com>
2026-05-08 13:56:20 +0600

    fix: remove misplaced handleGoogleLogin from auth-client, clean up exports

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -4,10 +4,4 @@ export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
 });
 
-const handleGoogleLogin = async () => {
-  await authClient.signIn.social({
-    provider: "google",
-    callbackURL: "/",
-  });
-};
-export const { signIn, signUp, signOut, useSession } = authClient;  
+export const { signIn, signUp, signOut, useSession } = authClient;
