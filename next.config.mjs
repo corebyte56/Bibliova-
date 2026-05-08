8637f612e7d7b792666d8fb9e5573360d6868de6
shahariar ahammad fahim <fahim789m@gmail.com>
2026-05-08 11:38:16 +0600

    fix: allow all external image domains in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactCompiler: true,
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "m.media-amazon.com",
-      },
-      {
-        protocol: "https",
-        hostname: "covers.openlibrary.org",
-      },
-      {
-        protocol: "https",
-        hostname: "i.ibb.co.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.google.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        hostname: "**",
       },
     ],
   },
